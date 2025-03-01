85e4ac14635ebb609579fb78bb19299d874ad6e5
sampathmenuka <sampath.m.chandimal99@gmail.com>
2025-03-01 14:45:58 +0530

    added js new fuctions

---
+++ script.js
@@ -0,0 +1,30 @@
+let dateConatiner = document.querySelector("date-container");
+let hoursConatiner = document.querySelector(".hours");
+let minitesConatiner = document.querySelector(".minites");
+let secondsConatiner = document.querySelector(".seconds");
+
+const weekdays =[
+    "Sunday",
+    "Monday",
+    "Thuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+cons monthNmaes = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "Auguest",
+    "September",
+    "Octomber",
+    "November",
+    "December"
+
+];
